3da680714e305cf553a6d2ad9833bb67a1f1e71d
krutika.thakkar <thakkarkrutika27@gmail.com>
2024-09-17 14:54:33 +0530

    Some refactoring, navbar component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent],
+  imports: [RouterOutlet, RouterLink, HomeComponent, AboutComponent, ProjectsComponent, ContactComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
